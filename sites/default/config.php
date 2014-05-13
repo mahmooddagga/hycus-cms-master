a0782417baad15e83bdaf3a54b6f0da12cacd2b9
mahmooddagga <mahmood.dagga@hotmail.com>
2014-05-13 19:24:05 +0300

    ghg

---
+++ sites/default/config.php
@@ -0,0 +1,10 @@
+<?php
+			defined( "HYCUSPAGEPROTECT" ) or die( "You don't have permission to view this page." );
+
+			$dbhost = "localhost";
+			$dbuser = "root";
+			$dbpass = "123456";
+			$dbname = "blogg";
+			$dbtype = "MySQL";
+			$dbprefix = "hycus_";
+			?>
